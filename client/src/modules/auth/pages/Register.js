70e920ddb2142019f32b8ca18570531b28c8acf9
Animesh Risal <animeshrisal99@gmail.com>
2021-08-05 17:13:51 +0545

    Update register page

--- client/src/modules/auth/pages/Register.js
+++ client/src/modules/auth/pages/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { authenticationService } from "../AuthService";
 import { useMutation } from "react-query";
+import { Form, Input, Button } from "antd";
 
 const Register = () => {
   const mutation = useMutation((user) => authenticationService.register(user));
@@ -13,10 +14,75 @@ const Register = () => {
       </div>
     );
   }
+  const onFinish = (values) => {
+    mutation.mutate(values);
+  };
 
   return (
     <div>
-      <h1>Sign Up</h1></div>
+      <h1>Login</h1>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+
+                return Promise.reject(
+                  new Error("The two passwords that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
