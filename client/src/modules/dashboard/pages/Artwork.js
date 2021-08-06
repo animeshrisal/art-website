ad8ce1aba553a0d6a7b59785e81b7b22dac6fe05
Animesh Risal <animeshrisal99@gmail.com>
2021-08-06 21:36:08 +0545

    Show comments

--- client/src/modules/dashboard/pages/Artwork.js
+++ client/src/modules/dashboard/pages/Artwork.js
@@ -2,13 +2,45 @@ import React from "react";
 import { useQuery } from "react-query";
 import { dashboardService } from "../DashboardService";
 import { useParams } from "react-router-dom";
-import { Image } from 'antd';
+import { Comment, Image, Tooltip } from "antd";
+import Avatar from "antd/lib/avatar/avatar";
+import moment from "moment";
 
 const Artwork = () => {
   const { id } = useParams();
   const { isLoading, data } = useQuery(["artwork", id], () =>
     dashboardService.artwork(id)
   );
+
+  const { isLoading: isLoadingComments, data: comments } = useQuery(
+    ["comments", id],
+    () => dashboardService.artComments(id)
+  );
+
+  const CommentContainer = () => {
+    if (isLoadingComments) {
+      return <div>Loading...</div>;
+    } else {
+      return comments.results.map((comment) => (
+        <Comment
+          key={comment.id}
+          avatar={
+            <Avatar
+              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+              alt="Han Solo"
+            />
+          }
+          author={<a>{comment.commentor}</a>}
+          content={<p>{comment.comment}</p>}
+          datetime={
+            <Tooltip title={moment().format("YYYY-MM-DD HH:mm:ss")}>
+              <span>{moment().fromNow()}</span>
+            </Tooltip>
+          }
+        />
+      ));
+    }
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -20,6 +52,7 @@ const Artwork = () => {
         <li key={data.id}>
           <Image width={200} src={data.image} />
         </li>
+        <CommentContainer />
       </div>
     );
   }
