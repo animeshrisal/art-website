c08ca14cb82bc318a6c8fc732d079ccd6dbfc33f
Animesh Risal <animeshrisal99@gmail.com>
2021-08-06 20:16:39 +0545

    Go to detail page on click

--- client/src/modules/dashboard/pages/Feed.js
+++ client/src/modules/dashboard/pages/Feed.js
@@ -3,13 +3,22 @@ import { useQuery } from "react-query";
 import { dashboardService } from "../DashboardService";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { useRouteMatch } from "react-router-dom";
 
-const Feed = () => {
+const Feed = (props) => {
+  
   const { isLoading, data } = useQuery("getFeed", dashboardService.getFeed);
+  
+  let { url } = useRouteMatch();
+
+  const goToDetailPage = (id) => {
+    console.log(url)
+    props.history.push(`${url}/artwork/${id}`);
+  }
 
   const renderImage = (image) => {
     return (
-      <li key={image.id}>
+      <li key={image.id} onClick={()=>goToDetailPage(image.id)}>
           <FontAwesomeIcon icon={faCoffee} />
         {image.id} <img src={image.image} alt={image.name} width="150px" height="150px" />
       </li>
