a575eef47cf6d4dfc1c401554916505c67694b93
Animesh Risal <animeshrisal99@gmail.com>
2022-01-31 17:43:14 +0545

    Fix asgi issue

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -6,20 +6,22 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
-
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 import os
-
+from django import setup
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
-from django.core.asgi import get_asgi_application
+
 from dashboard import consumers
 from .middleware import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+setup()
 
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
 
     # WebSocket chat handler
     "websocket": TokenAuthMiddleware(
